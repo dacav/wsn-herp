0eb05a9747e6a6f34a563de112351f69c0a815fd
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2012-07-30 16:32:52 +0200

    Fix in project structure

--- src/headers/herp.h
+++ src/headers/herp.h
@@ -10,39 +10,43 @@ enum {
 
 /** Type identifiers for messages. */
 typedef enum {
-    TYPE_REACH  = 0x01,  /**< Path discovery (broadcasted). */
-    TYPE_PATH_B = 0x02,  /**< Reverse path building. */
-    TYPE_PATH_V = 0x04,  /**< Forward path verification. */
-    TYPE_MESSG  = 0x08   /**< User payload messages. */
+    HERP_TYPE_REACH  = 0x01,  /**< Path discovery (broadcasted). */
+    HERP_TYPE_PATH_B = 0x02,  /**< Reverse path building. */
+    HERP_TYPE_PATH_V = 0x04,  /**< Forward path verification. */
+    HERP_TYPE_MSG    = 0x08   /**< User payload messages. */
 } herp_type_t;
 
 typedef nx_struct {
-
     nx_uint8_t type;
-
+    nx_uint8_t id;
     nx_am_addr_t from;
     nx_am_addr_t to;
-    nx_uint8_t id;
+} herp_head_t;
+
+typedef nx_struct {
+
+    herp_head_t header;
 
     nx_union {
 
-        /** To be used when nx_struct::type is herp_type_t::TYPE_REACH. */
+        /** To be used when nx_struct::type is
+         * herp_type_t::HERP_TYPE_REACH. */
         nx_struct {
             nx_uint16_t hop_count;  /**< Hops to source. */
             nx_uint16_t max_hop;    /**< Max distance in hops. */
             nx_am_addr_t previous;  /**< Previous hop. */
         } reach;
 
-        /** To be used when nx_struct::type is herp_type_t::TYPE_PATH_B or
-         * herp_type_t::TYPE_PATH_V. */
+        /** To be used when nx_struct::type is
+         * herp_type_t::HERP_TYPE_PATH_B or
+         * herp_type_t::HERP_TYPE_PATH_V. */
         nx_struct {
-            nx_uint8_t n_hops;
             nx_am_addr_t hops[0];
         } path;
 
-        /** To be used when nx_struct::type is herp_type_t::TYPE_MESSG */
+        /** To be used when nx_struct::type is
+         * herp_type_t::HERP_TYPE_MSG */
         nx_struct {
-            nx_uint8_t n_bytes;
             nx_uint8_t user_payload[0];
         } msg;
 
