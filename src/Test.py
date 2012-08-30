cd84282b6ee99c8c7b8639dcbfa0e63378b81a1c
Giovanni [dacav] Simoni <simgidacav@gmail.com>
2012-08-30 05:22:45 -0400

    Test application supporting fed-in topology

--- src/Test.py
+++ src/Test.py
@@ -10,10 +10,15 @@ except: pass
 
 from TOSSIM import *;
 
+NOISE_FILE = os.path.join (
+    os.environ['TOSROOT'],
+    'tos/lib/tossim/noise/meyer-heavy.txt'
+)
+
 def load_noise (T, nodes_id):
-    noise = open(os.environ['TOSROOT'] +
-                 '/tos/lib/tossim/noise/meyer-heavy.txt')
-    for line in noise:
+    noise_file = open(NOISE_FILE)
+    nodes_id = list(nodes_id)
+    for line in noise_file:
         s = line.strip()
         if (s):
             val = int(s)
@@ -21,29 +26,80 @@ def load_noise (T, nodes_id):
                 T.getNode(i).addNoiseTraceReading(val)
     for i in nodes_id:
         T.getNode(i).createNoiseModel()
-    noise.close()
+    noise_file.close()
+
+class Topology :
+
+    @staticmethod
+    def ring (N) :
+        assert(N > 0)
+        def sequence ():
+            for i in range(N - 1):
+                yield (i, i + 1)
+            yield (N - 1, 0)
+        return Topology(sequence())
+
+    @staticmethod
+    def load_file (FileName) :
+        F = open(FileName, 'rt')
+        lines = (line[:-1].split() for line in F.readlines())
+        F.close()
+        return Topology(((int(x), int(y)) for (x, y) in lines))
+
+    def __init__ (self, sequence):
+        self.nodes = dict()
+        nodes = set()
+        links = set()
+        for (x, y) in sequence:
+            nodes.update((x, y))
+            links.add((x, y))
+        self.nodes = nodes
+        self.links = links
+
+    def get_nodes (self):
+        return iter(self.nodes)
+
+    def get_links (self):
+        for x, y in self.links:
+            yield x, y
+            yield y, x
 
 def main (argv=None):
 
-    NODE_COUNT = 1;
+    NODE_COUNT = 8;
+
+    topology = Topology.load_file('topology')
+
+    log = open('what-happens.txt', 'wt')
 
     T = Tossim([])
-    T.addChannel('Out', sys.stdout)
+    T.addChannel('Prot', log)
+    T.addChannel('RTab', log);
+    T.addChannel('Out', sys.stdout);
 
     R = T.radio()
 
-    nodes_id = list( range(NODE_COUNT) )
-    load_noise(T, nodes_id)
-    [R.add(x, y, -80) for x in nodes_id for y in nodes_id]
+    load_noise(T, topology.get_nodes())
 
-    for ni in nodes_id:
+    for x, y in topology.get_links():
+        print("Linking ", x, "and", y)
+        R.add(x, y, -40)
+
+    for ni in topology.get_nodes():
         n = T.getNode(ni);
         n.turnOn()
 
-    for i in it.count():
-        T.runNextEvent();
-        print("step", i)
-        time.sleep(0.25)
+    try:
+        for i in it.count():
+            T.runNextEvent();
+            #time.sleep(0.25)
+            if i == 3000:
+                break;
+    except KeyboardInterrupt:
+        print('Terminated.')
+    finally:
+        log.close()
+        print('Also everything fine...')
 
 if __name__ == '__main__':
     sys.exit(main())
